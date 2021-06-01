68d53cebf058898ed994b33fd29c562edac04ab2
Чебыкин Андрей Геннадьевич <31141640+AndrGen@users.noreply.github.com>
2021-06-01 17:44:39 +0300

    1

---
+++ src/Web/ProcessSystem.UI.Blazor/Server/Controllers/RegisterController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProcessSystem.UI.Blazor.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProcessSystem.UI.Blazor.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RegisterController : ControllerBase
+    {
+
+        private readonly ILogger<RegisterController> _logger;
+
+        public RegisterController(ILogger<RegisterController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Test";
+        }
+    }
+}
